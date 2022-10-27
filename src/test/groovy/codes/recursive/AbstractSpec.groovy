a8628de2c7a152185368ca5d8c57b39faaee4e8c
moviegoerholly <moviegoerholly@gmail.com>
2022-10-27 16:14:11 +0530

    Publish Test Report

---
+++ src/test/groovy/codes/recursive/AbstractSpec.groovy
@@ -0,0 +1,9 @@
+package codes.recursive
+
+import io.micronaut.context.ApplicationContext
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+class AbstractSpec extends Specification {
+}
---
+++ src/test/groovy/codes/recursive/HelloWorldSpec.groovy
@@ -0,0 +1,22 @@
+package codes.recursive
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.*
+
+@MicronautTest
+class HelloWorldSpec extends AbstractSpec {
+    def "test hello world"() {
+        def foo = 'bar'
+        when:
+            foo == 'bar'
+        then:
+            foo.reverse() == 'rab'
+    }
+
+    def "test failure"() {
+        when:
+            true == true
+        then:
+            false == true
+    }
+}
